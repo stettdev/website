b6f7223f65a5417193148bc56367291166ba66cd
Krzysztof Gocek <krzysztof.gocek@protonmail.com>
2022-01-07 16:53:10 +0100

    Desktop Version

--- assets/scripts/topbar.js
+++ assets/scripts/topbar.js
@@ -4,27 +4,36 @@ const topbar = document.querySelector('#topbar');
 const websitePages = document.querySelectorAll('.page');
 const topbarLinks = topbar.querySelectorAll('a');
 
-const topbarOptions = {
-  threshold: 0.125,
-};
+function detectMobile() {
+  return (window.innerWidth <= 800 );
+}
 
-const fromNextPage = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) topbar.classList.add('visible');
-    else topbar.classList.remove('visible');
+if (!detectMobile()) {
+  const topbarOptions = {
+    threshold: 0.125,
+  };
+
+  const fromNextPage = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) topbar.classList.add('visible');
+      else topbar.classList.remove('visible');
+    });
+  }, topbarOptions);
+
+  fromNextPage.observe(homePage);
+
+  const indicate = new IntersectionObserver((entries) => {
+    entries.map((e) => {
+      if (!e.isIntersecting) return;
+      topbarLinks.forEach((link) => link.classList.remove('current'));
+      topbar.querySelector(`a[href="#${e.target.id}"]`).classList.add('current');
+    });
+  }, topbarOptions);
+
+  websitePages.forEach((page) => {
+    indicate.observe(page);
   });
-}, topbarOptions);
 
-fromNextPage.observe(homePage);
-
-const indicate = new IntersectionObserver((entries) => {
-  entries.map((e) => {
-    if (!e.isIntersecting) return;
-    topbarLinks.forEach((link) => link.classList.remove('current'));
-    topbar.querySelector(`a[href="#${e.target.id}"]`).classList.add('current');
-  });
-}, topbarOptions);
-
-websitePages.forEach((page) => {
-  indicate.observe(page);
-});
+} else {
+  topbar.classList.add('visible');
+}
